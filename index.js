9d8e71013a322010bf743a3048af9705f58daabe
Alex Edelstein <alex@edelstein.org>
2016-10-18 16:45:15 -0700

    testing impact

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
-  var i = 0;
+  //var i = 0;
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.text) {
