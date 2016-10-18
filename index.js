e84f0b2ea778e707f12ce470753b5fcaeaaa87b6
Alex Edelstein <alex@edelstein.org>
2016-10-18 16:42:13 -0700

    add var i

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
-  for (i=0; i < events.length; i++) {
+  var i = 0;
+  for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.text) {
       sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
