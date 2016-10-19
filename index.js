c87fee39ffa634545b66de768000522e78d164e2
Alex Edelstein <alex@edelstein.org>
2016-10-18 17:02:04 -0700

    added support for kittens

--- index.js
+++ index.js
@@ -63,11 +63,12 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
-  //var i = 0;
+  var i = 0;
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.text) {
-      sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+      if (!kittenMessage(event.sender.id, event.message.text)) {
+        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
     }
     res.sendStatus(200);
   }
@@ -89,4 +90,42 @@ function sendMessage(recipientId, message) {
         }
       }
     });
-};
+  };
+
+  function kittenMessage(recipient) {
+    var text = text || "";
+    var values  = text.split(' ');
+
+    if (values.length === 3 && values[0] === 'kitten') {
+      if (Number(values[1]) > 0 && Number(values[2]) > 0) {
+        var imageURL = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
+
+        message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Kitten",
+                            "subtitle": "Cute kitten picture",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show kitten"
+                                }, {
+                                "type": "postback",
+                                "title": "I like this",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        }]
+                    }
+                }
+            };
+
+        sendMessage(recipientId, message);
+        return true;
+      }
+    }
+      return false;
+  };
