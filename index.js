7ea6e3a31d1ce19651c8e1c3eb0e109da5f011db
Alex Edelstein <alex@edelstein.org>
2016-10-18 17:12:59 -0700

    fix

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
   var i = 0;
+  sendMessage(events[0].sender.id, {text: "foo"});
+
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     if (event.message && event.message.text) {
