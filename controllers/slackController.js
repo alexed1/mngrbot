b0ff2621cfdd855053a2928abc3f83d7a7b8eb1c
Alexei Avrutin <alexavrutin@gmail.com>
2016-11-04 13:45:14 +0100

    Fix, welcome message for any channel

--- controllers/slackController.js
+++ controllers/slackController.js
@@ -8,15 +8,17 @@
 
 'use strict';
 
-var express = require('express'),
-    router = express.Router();
+const express = require('express'),
+    router = express.Router(),
+    empty_line = "   ";
+
 
 // Handles clicking on the buttons within a Slack message.
 router.post('/', (req, res) => {
-    let pmBot = req.app.get('pmBot')
-    var payload = JSON.parse(req.body.payload);
+    let pmBot = req.app.get('pmBot'),
+    payload = JSON.parse(req.body.payload);
     pmBot.emit('buttonClicked', payload);
-    res.status(200).send("   ");
+    res.status(200).send(empty_line);
 });
 
 module.exports = router;
